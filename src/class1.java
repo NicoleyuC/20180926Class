91744830abc403f37627e6f0972002ffca6565c6
NicoleyuC <43199808+NicoleyuC@users.noreply.github.com>
2018-09-26 19:19:54 +0800

    Initial commit

---
+++ src/class1.java
@@ -0,0 +1,10 @@
+// 字元 轉換 ASCII碼
+import java.util.Scanner;
+
+public class class1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char Ch = sc.next().charAt(0);
+        System.out.println((int)Ch);
+    }
+}
